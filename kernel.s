8ecc32698e1c4793beecb275eb16bf56950b58a9
Orrexon <orrexon@gmail.com>
2021-01-04 03:24:41 +0100

    new branch for development

--- kernel.s
+++ kernel.s
@@ -1,6 +1,42 @@
-.text
+.section .text
 .global main_asm
 
 main_asm:
+	bl uart_init
+	x1, =hello_world
+	bl uart_writeText
+
 	while_true:
 		b while_true
+
+.section .data
+hello_world: .ascii "Hello world\n"
+
+.section .text
+.global uart_init
+uart_init:
+	
+
+.global uart_writetext
+uart_writetext:
+
+
+.section .data
+.balign 4
+PERIPH_BASE 	.word 0xFE000000
+.balign 4
+GPFSEL0_OFF 	.word 0x200000
+.balign 4
+GPSET0_OFF 	.word 0x20001C
+.balign 4
+GPCLR0_OFF 	.word 0x200028
+.balign 4
+GPPUPPDN0_OFF 	.word 0x2000E4
+
+.balign 4
+GPIO_MAX_PIN 		.word 53
+.balign 4
+GPIO_FUNCTION_ALT5 	.word 2
+
+.balign 4
+PULL_NONE .word 0
