ed709f79993bd61fad48cf0ae40ec0a93b5a59ff
Orrexon <orrexon@gmail.com>
2021-02-21 12:32:34 +0100

    New boot code test 1

--- boot.s
+++ boot.s
@@ -3,6 +3,91 @@
 .global _start
 
 _start:
+    // read cpu id, stop slave cores
+    mrs     x1, mpidr_el1
+    and     x1, x1, #3
+    cbz     x1, TWO
+    // cpu id > 0, stop
+ONE:  wfe
+    b       ONE
+TWO:  // cpu id == 0
+
+    // set stack before our code
+    ldr     x1, =_start
+
+    // set up EL1
+    mrs     x0, CurrentEL
+    and     x0, x0, #12 // clear reserved bits
+
+    // running at EL3?
+    cmp     x0, #12
+    bne     FIVE1
+    // should never be executed, just for completeness
+    mov     x2, #0x5b1
+    msr     scr_el3, x2
+    mov     x2, #0x3c9
+    msr     spsr_el3, x2
+    adr     x2, FIVE1
+    msr     elr_el3, x2
+    eret
+
+    // running at EL2?
+FIVE1:  cmp     x0, #4
+    beq     FIVE2
+    msr     sp_el1, x1
+    // enable CNTP for EL1
+    mrs     x0, cnthctl_el2
+    orr     x0, x0, #3
+    msr     cnthctl_el2, x0
+    msr     cntvoff_el2, xzr
+    // enable AArch64 in EL1
+    mov     x0, #(1 << 31)      // AArch64
+    orr     x0, x0, #(1 << 1)   // SWIO hardwired on Pi3
+    msr     hcr_el2, x0
+    mrs     x0, hcr_el2
+    // Setup SCTLR access
+    mov     x2, #0x0800
+    movk    x2, #0x30d0, lsl #16
+    msr     sctlr_el1, x2
+    // set up exception handlers
+    ldr     x2, =_vectors
+    msr     vbar_el1, x2
+    // change execution level to EL1
+    mov     x2, #0x3c4
+    msr     spsr_el2, x2
+    adr     x2, FIVE2
+    msr     elr_el2, x2
+    eret
+
+FIVE2:  mov     sp, x1
+
+    // clear bss
+    ldr     x1, =__bss_start
+    ldr     w2, =__bss_size
+THREE:  cbz     w2, FOUR
+    str     xzr, [x1], #8
+    sub     w2, w2, #1
+    cbnz    w2, THREE
+
+FOUR:	
+	#jump to program
+//	bl main_asm
+	bl main
+	b ONE
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+_start:
 //check processor ID == 0 to execute on main core
 
 	mrs x1, mpidr_el1
@@ -67,7 +152,7 @@ FOUR:
 	b ONE
 
 
-
+*/
 
 
 .align 11
